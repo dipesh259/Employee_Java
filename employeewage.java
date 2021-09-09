28dc7241d9b114e214f42a5c475383dc76bb3098
sick3356 <myselfdipu259@gmail.com>
2021-09-09 18:54:12 +0530

    Check present or absent

---
+++ employeewage.java
@@ -0,0 +1,10 @@
+public class emp {
+    public static void main(String[] args) {
+            int rand = (int) (Math.random() * 10 / 2);
+            if (rand > 0)
+                System.out.println("Employee is present");
+            else
+                System.out.println("Employee is absent");
+    }
+
+}
