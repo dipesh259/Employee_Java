67edfe8219cebb60c354986e453794a4c416f0e5
sick3356 <myselfdipu259@gmail.com>
2021-09-13 09:10:22 +0530

    UsingSwitchCase

--- employeewage.java
+++ employeewage.java
@@ -1,19 +1,25 @@
 package learned;
 
-import java.util.Scanner;
-
-public class partime {
+public class Swcla {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int p;
-
-        //Input data
-        System.out.println("Enter wage per hour: ");
-        int w = sc.nextInt();
-        System.out.println("Enter total hour of part-time work: ");
-        int h = sc.nextInt();
-
-        p = w * h;
-        System.out.println("Part-time wage is: "+p);
+        final int p = 1;
+        final int f = 2;
+        final int w = 20;
+        int emptype = (int) (Math.random()*100)%3;
+        int work = 0;
+        switch (emptype){
+            case f:
+                System.out.println("Employee is Present ");
+                work = 8;
+                break;
+            case p:
+                System.out.println("Employee is present Part time");
+                work = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+        }
+        int wag = work * w;
+        System.out.println("Employee Daily wage is " + wag);
     }
 }
