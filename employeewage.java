f810547e97c35512c5153bcfebf5917ecdd79b45
sick3356 <myselfdipu259@gmail.com>
2021-09-13 09:14:39 +0530

    Monthly

--- employeewage.java
+++ employeewage.java
@@ -1,25 +1,23 @@
 package learned;
 
-public class Swcla {
+import java.util.Scanner;
+
+public class monthly {
     public static void main(String[] args) {
-        final int p = 1;
-        final int f = 2;
-        final int w = 20;
-        int emptype = (int) (Math.random()*100)%3;
-        int work = 0;
-        switch (emptype){
-            case f:
-                System.out.println("Employee is Present ");
-                work = 8;
-                break;
-            case p:
-                System.out.println("Employee is present Part time");
-                work = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
-        }
-        int wag = work * w;
-        System.out.println("Employee Daily wage is " + wag);
+        Scanner sc = new Scanner(System.in);
+        int total, month;
+
+        //Input data
+        System.out.println("Enter wage per hour: ");
+        int w = sc.nextInt();
+        System.out.println("Enter total hour of work: ");
+        int h = sc.nextInt();
+        System.out.println("Enter number of working days: ");
+        int m = sc.nextInt();
+
+        //calculate the wage
+        total = w * h;
+        month = total * m;
+        System.out.println("Total wage is: "+month);
     }
 }
